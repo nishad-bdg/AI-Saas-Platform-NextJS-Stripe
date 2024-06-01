831d00bfc9f6d970a77e890ad933c25eb9a83d67
Nishad Islam <nishad.nik@gmail.com>
2024-06-01 12:53:49 +0600

    clerk middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", '/api/webhooks/clerk']);
+// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", '/api/webhooks/clerk']);
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
